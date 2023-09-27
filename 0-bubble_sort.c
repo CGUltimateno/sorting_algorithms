e67b412e7a0032a0ea3baadbf08770b0e594fc5a
CGUltimateno <Mohameddeaf65@gmail.com>
2023-09-27 10:29:43 +0300

    0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,26 +9,26 @@
 
 void bubble_sort(int *array, size_t size)
 {
-    int i=0, j=0;
-    int temp = 0;
-    int pass_required = 0, last_index = 0;
+int i = 0, j = 0;
+int temp = 0;
+int pass_required = 0, last_index = 0;
 
-    if (array == NULL || size < 2)
-        return;
+if (array == NULL || size < 2)
+return;
 
-    pass_required = size - 1;
-    for (i = 0; i < pass_required; i++)
-    {
-        last_index = size - i - 1;
-        for (j = 0; j < last_index; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-                print_array(array, size);
-            }
-        }
-    }
+pass_required = size - 1;
+for (i = 0; i < pass_required; i++)
+{
+last_index = size - i - 1;
+for (j = 0; j < last_index; j++)
+{
+if (array[j] > array[j + 1])
+{
+temp = array[j];
+array[j] = array[j + 1];
+array[j + 1] = temp;
+print_array(array, size);
+}
+}
+}
 }
