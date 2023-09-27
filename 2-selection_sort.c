3f124da976332b9325bf0768a8fafd7517d25bfb
CGUltimateno <Mohameddeaf65@gmail.com>
2023-09-27 10:33:33 +0300

    2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,29 +8,29 @@
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t min_size = 0;
-    size_t pass = 0, index = 0;
-    size_t pass_required = 0;
-    int temp = 0;
+size_t min_size = 0;
+size_t pass = 0, index = 0;
+size_t pass_required = 0;
+int temp = 0;
 
-    if (array == NULL || size < 2)
-        return;
+if (array == NULL || size < 2)
+return;
 
-    pass_required = size - 1;
-    for (pass = 0; pass < pass_required; pass++)
-    {
-        min_size = pass;
-        for (index = pass + 1; index < size; index++)
-        {
-            if (array[index] < array[min_size])
-                min_size = index;
-        }
-            temp = array[pass];
-            array[pass] = array[min_size];
-            array[min_size] = temp;
-            if (min_size != pass)
-            {
-                print_array(array, size);
-        }
-    }
+pass_required = size - 1;
+for (pass = 0; pass < pass_required; pass++)
+{
+min_size = pass;
+for (index = pass + 1; index < size; index++)
+{
+if (array[index] < array[min_size])
+min_size = index;
+}
+temp = array[pass];
+array[pass] = array[min_size];
+array[min_size] = temp;
+if (min_size != pass)
+{
+print_array(array, size);
+}
+}
 }
